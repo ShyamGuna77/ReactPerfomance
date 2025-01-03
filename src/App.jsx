4d8411f07e1b20f1dd955202f5f8f72c28c86c4f
Shyam <gshyamprasad2001@gmail.com>
2025-01-03 17:12:01 +0530

    Added

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+import Counter from './components/Counter/Counter.jsx';
+import Header from './components/Header.jsx';
+import { log } from './log.js';
+import ConfigRender from './components/Counter/ConfigRender.jsx';
+
+function App() {
+  log('<App /> rendered');
+
+  
+  const [chosenCount, setChosenCount] = useState(0);
+   function handleSetCount(number){
+    setChosenCount(number);
+   }
+  
+  return (
+    <>
+      <Header />
+      <main>
+        <ConfigRender onSet = {handleSetCount}/>
+        <Counter initialCount={chosenCount} />
+      </main>
+    </>
+  );
+}
+
+export default App;
